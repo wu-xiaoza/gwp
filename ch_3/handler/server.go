4ec5bdcf6b0c9c87dc53ebbee1212d3cb64b251f
吴小杂 <wuxiaozade@126.com>
2021-01-04 21:56:19 +0800

    提醒

---
+++ ch_3/handler/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//MyHandler .
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello worlld!")
+}
+
+func main() {
+	handler := MyHandler{}
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: &handler,
+	}
+	server.ListenAndServe()
+}
